aedcc1e09c045e065d7eed56cfa38c99f439cdc3
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-09-24 11:20:08 +0800

    latest changes

--- server.js
+++ server.js
@@ -1,65 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const favicon = require('serve-favicon');
+const path = require('path');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var user = process.env.GMAIL_USERNAME;
-var secret_key = process.env.GMAIL_SECRET_KEY;
-
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // secure:true for port 465, secure:false for port 587
-    auth: {
-        user: user,
-        pass: secret_key
-    }
-});
 
 
 app.get('/', (request, response) => {
     response.render('index.hbs');
 });
 
-app.post('/form-submit', (request, response) => {
-    
-    let senderName = request.body['name'],
-        senderEmail = request.body['email'],
-        senderMessage = request.body['message'],
-
-    // setup email data with unicode symbols
-        mailOptions = {
-            from: senderEmail, // sender address
-            to: user, // list of receivers
-            subject: `New message from ${senderName} on kowshiksundararajan.com`, // Subject line
-            text: senderMessage, // plain text body
-            html: `<p>Name: <b>${senderName}</b><br />
-                    Email: ${senderEmail}</p>
-                    <p>Message: ${senderMessage}</p>` // html body
-        };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            response.send(error);
-            return;
-        }
-        console.log('* message %s sent: %s', info.messageId, info.response);
-        response.send('Your email has been sent, I will get back to you within 24 hours.');
-    });
-
-    
-});
+app.get('/resume', (request, response) => {
+    var data = fs.readFileSync('./public/Kowshik_Sundararajan_Resume.pdf');
+    // response.setHeader('Kowshik Sundararajan CV');
+    response.contentType('application/pdf');
+    response.send(data);
+})
 
 app.listen(port, () => {
     console.log(`* app started on port ${port}`);
